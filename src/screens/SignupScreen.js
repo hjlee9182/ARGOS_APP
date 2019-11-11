d0cdf11dc19b01a2d8131a6732be6bd6d9666bf6
Hyunjun Lee <zxzxs9182@gmail.com>
2019-11-12 00:34:11 +0900

    text to move signin

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import {View,StyleSheet} from 'react-native';
+import React,{useState} from 'react';
+import {View,StyleSheet,TouchableOpacity} from 'react-native';
 import {Text,Input,Button} from 'react-native-elements';
 import Spacer from '../components/Spacer';
 const SignupScreen = ({navigation}) =>{
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
     return (
     <View style={styles.container}>
     <Spacer>
     <Text h3> Sign Up for ARGOS</Text>
     </Spacer>
-    <Input label="Email"/>
+    <Input 
+    label="Email" 
+    value={email}  
+    onChangeText = {setEmail}
+    autoCapitalize="none"
+    autoCorrect={false} />
     <Spacer/>
-    <Input label="Password"/>
+    <Input 
+    secureTextEntry
+    label="Password"
+    value={password}
+    onChangeText= {setPassword}
+    autoCapitalize="none"
+    autoCorrect={false}
+    />
     <Spacer>
     <Button title="Sign up"/>
     </Spacer>
+    <TouchableOpacity 
+    onPress={()=>navigation.navigate('Signin')}>
+        <Spacer>
+    <Text style={styles.link}>Already have an account? Sign in instead</Text>
+        </Spacer>
+    </TouchableOpacity>
     </View>
     );
 };
@@ -29,7 +50,11 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:'center',
     marginBottom:250
+    },
+    link:{
+        color:'blue'
     }
+    
 });
 
 export default SignupScreen;
