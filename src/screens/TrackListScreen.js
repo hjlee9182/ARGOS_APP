2f4805f2fc52759219cb93517528051bd8dffca0
Hyunjun Lee <zxzxs9182@gmail.com>
2019-11-11 23:40:44 +0900

    Make button to move another components

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {View,StyleSheet,Text} from 'react-native';
+import {View,StyleSheet,Text,Button} from 'react-native';
 
-const TrackListScreen = () =>{
-    return <Text style = {{fontSize:48}}>TrackListScreen</Text>
+const TrackListScreen = ({navigation}) =>{
+    return <>
+    <Text style = {{fontSize:48}}>TrackListScreen</Text>
+    <Button 
+    title ="Go to Track Detail"
+    onPress={()=>navigation.navigate('TrackDetail')}></Button>
+    </>
 };
 
 const styles = StyleSheet.create({});
