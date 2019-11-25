313139c9755e44f2563ea19fe13495de449dd6b8
Hyunjun Lee <zxzxs9182@gmail.com>
2019-11-26 02:24:20 +0900

    Add icon in navigation

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import PersonalInformation from './src/screens/PersonalInformation';
 import SigninScreen from './src/screens/SigninScreen';
 import SignupScreen from './src/screens/SignupScreen';
@@ -11,6 +10,27 @@ import BoardScreen from './src/screens/BoardScreen';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName === '푸시알람') {
+    iconName = `ios-paper-plane${focused ? '' : ''}`;
+  }
+  else if (routeName === '마이페이지') {
+    iconName = `ios-person${focused ? '' : ''}`;
+  }
+  else if (routeName === '게시판') {
+    iconName = `ios-clipboard${focused ? '' : ''}`;
+  }
+  else if (routeName === '세미나') {
+    iconName = `ios-create${focused ? '' : ''}`;
+  }
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
 
 const switchNavigator = createSwitchNavigator({
   loginFlow : createStackNavigator({
@@ -25,7 +45,14 @@ const switchNavigator = createSwitchNavigator({
     세미나 : SeminarScreen,
     마이페이지:PersonalInformation,
     푸시알람 : PushAlarmScreen
-  })
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor),
+    }
+  )},
+  )
 });
 
 export default createAppContainer(switchNavigator);
